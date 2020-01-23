cf34090d1b846e4c2d6096717e78868808b01e0b
David Uldrich <David.Uldrich@outlook.com>
2020-01-23 16:55:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace D1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause display to Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
