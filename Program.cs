46cb0a59c3aa896f3024020593d936d4b433d817
David Uldrich <David.Uldrich@outlook.com>
2020-01-23 17:36:40 -0500

    Added comment for Hello World message

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace D1_HelloWorld
         {
             // Pause display to Hello World!
             Console.WriteLine("Press any key to continue...");
+
+            // Display the message "Hello World!" message
             Console.WriteLine("Hello World!");
             Console.ReadKey(true);
         }
